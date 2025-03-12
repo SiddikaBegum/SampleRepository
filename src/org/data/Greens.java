51b8934dd52892dcdc962ce79b6e79dbc274be35
SiddikaBegum <siddikabegum29@gmail.com>
2025-03-12 15:12:33 +0530

    2nd commit

---
+++ src/org/data/Greens.java
@@ -0,0 +1,12 @@
+package org.data;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Greens {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.greenstechnology.com/");
+	}
+
+}
---
+++ src/org/data/Student.java
@@ -0,0 +1,15 @@
+package org.data;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+			}
+	}
+
+

de06934758e9c0f634913d311175905834c5f13c
SiddikaBegum <siddikabegum29@gmail.com>
2025-03-12 15:30:00 +0530

    Update Student.java

--- src/org/data/Student.java
+++ src/org/data/Student.java
@@ -9,7 +9,10 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
-System.out.println(3);
+
+		System.out.println(3);
+		System.out.println(4);
+		
 
 
 			}
